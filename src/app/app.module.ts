0f8302534d8e2a2126a6b4c4721cbff53eae54eb
sizzlorr <igron.tao@gmail.com>
2021-01-14 12:55:33 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FollowersListComponent } from './components/followers-list/followers-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FollowersListComponent
   ],
   imports: [
     BrowserModule
